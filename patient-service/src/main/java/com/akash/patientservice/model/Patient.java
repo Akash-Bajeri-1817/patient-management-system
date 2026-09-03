7392ef515a7e79fb7f9b54f87a1147cc044d4863
Akash-Bajeri-1817 <akashbajeri18@gmail.com>
2026-09-03 21:54:04 +0530

    patient service h2 db setup, testing data done

---
+++ patient-service/src/main/java/com/akash/patientservice/model/Patient.java
@@ -0,0 +1,81 @@
+package com.akash.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class Patient
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registrationDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public @NotNull String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    public @NotNull @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotNull @Email String email) {
+        this.email = email;
+    }
+
+    public @NotNull String getAddress() {
+        return address;
+    }
+
+    public void setAddress(@NotNull String address) {
+        this.address = address;
+    }
+
+    public @NotNull LocalDate getBirthDate() {
+        return dateOfBirth;
+    }
+
+    public void setBirthDate(@NotNull LocalDate birthDate) {
+        this.dateOfBirth = birthDate;
+    }
+
+    public @NotNull LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(@NotNull LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
